f805fe2b465472c4becb62d8c32d2c0b6e0f17cc
hbouaraour <hbouaraour@gmail.com>
2024-09-14 06:31:16 +0200

    transformer

--- lesFunOdrSup/src/main/kotlin/Main.kt
+++ lesFunOdrSup/src/main/kotlin/Main.kt
@@ -4,6 +4,18 @@ import fr.hamtec.boulang.cookies
 import fr.hamtec.odrSup.*
 
 fun main() {
+    affMap()
+}
+fun affMap(): Unit {
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+    println("Full menu:")
+    fullMenu.forEach {
+        println(it)
+    }
+}
+fun affForEach(): Unit {
     cookies.forEach {
         println("Menu item: ${it.name}")
     }
